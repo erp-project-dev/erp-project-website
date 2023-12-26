8a195c0469630c28efd931f735a1cbb208ea2bb1
eduardo <erodriguezp105@gmail.com>
2023-12-26 08:24:11 -0500

    feat: return google analytics

--- src/common/ga.js
+++
@@ -1,7 +0,0 @@
-import cfg from "./config";
-import ReactGA from "react-ga";
-
-ReactGA.initialize(cfg.GOOGLE_ANALYTICS_TRACKING_CODE);
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-export default ReactGA;
--- src/components/Content/components/FollowMe/index.js
+++ src/components/Content/components/FollowMe/index.js
@@ -1,15 +1,6 @@
-import ga from "../../../../common/ga";
 import { SocialIcon } from "react-social-icons";
 
 import "./index.scss";
-
-const gaClick = (category, action) => {
-  console.info(`${category}: ${action}`);
-  ga.event({
-    category: category,
-    action: action,
-  });
-};
 
 function FollowMe() {
   return (
@@ -17,30 +8,18 @@ function FollowMe() {
       <h2 className="title">Sígueme en</h2>
       <ul className="links">
         <li>
-          <SocialIcon
-            onClick={() => gaClick("FollowMe", "Click on Instagram")}
-            url="https://www.instagram.com/erpprojectofficial/"
-          />
+          <SocialIcon url="https://www.instagram.com/erpprojectofficial/" />
         </li>
         <li>
-          <SocialIcon
-            onClick={() => gaClick("FollowMe", "Click on YouTube")}
-            url="https://www.youtube.com/c/erpproject?sub_confirmation=1"
-          />
+          <SocialIcon url="https://www.youtube.com/c/erpproject?sub_confirmation=1" />
         </li>
         <li>
-          <SocialIcon
-            onClick={() => gaClick("FollowMe", "Click on Spotify")}
-            url="https://open.spotify.com/intl-es/artist/6xR1HRj6SzReee9R2EkOUo?si=5fa9cbe535da4ee6"
-          />
+          <SocialIcon url="https://open.spotify.com/intl-es/artist/6xR1HRj6SzReee9R2EkOUo?si=5fa9cbe535da4ee6" />
         </li>
       </ul>
       <p>
         o escríbeme a mi{" "}
-        <a
-          onClick={() => gaClick("FollowMe", "Click on E-mail")}
-          href="mailto:erodriguezp105@gmail.com"
-        >
+        <a href="mailto:erodriguezp105@gmail.com">
           <span className="has-text-primary">correo electrónico</span>
         </a>
       </p>
